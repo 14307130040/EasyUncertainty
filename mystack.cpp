ea35bfbaa0e32c9a51bb0d065dbf1698e143c255
xiao <jerry9605@hotmail.com>
2015-05-11 23:21:49 +0800

    bad-address error fixed

--- mystack.cpp
+++ mystack.cpp
@@ -6,26 +6,26 @@ template<class TYPE>
 class mystack
 {
 private:
-	TYPE elements[STACKMAX];
+    TYPE elements[STACKMAX];
 public:
-	TYPE* SS;	//stack segment
-	TYPE* SP;	//stack pointer
-	mystack()
-	{
-		SS = elements;
-		SP = &elements[STACKMAX];	//point behind the end of the arr
-	}
-	TYPE* push(TYPE element)
-	{
-		*(--SP) = element;
-		return SP;
-	}
+    TYPE* SS;	//stack segment
+    TYPE* SP;	//stack pointer
+    mystack()
+    {
+    	SS = elements;
+    	SP = &elements[STACKMAX];	//point behind the end of the arr
+    }
+    TYPE* push(TYPE element)
+    {
+    	*(--SP) = element;
+    	return SP;
+    }
 
-	TYPE* pop(TYPE& element)
-	{
-		element = *(SP++);
-		return SP;
-	}
+    TYPE* pop(TYPE& element)
+    {
+    	element = *(SP++);
+    	return SP;
+    }
 };
 
 #endif
--- uncertainty.cpp
+++ uncertainty.cpp
@@ -7,58 +7,58 @@ template<class TYPE>
 class uncertainty
 {
 public:
-	TYPE main;
-	TYPE uncert;
+    TYPE main;
+    TYPE uncert;
 public:
-	uncertainty(TYPE m, TYPE u)
-	{
-		this->main = m;
-		this->uncert = u;
-	}
+    uncertainty(TYPE m, TYPE u)
+    {
+    	this->main = m;
+    	this->uncert = u;
+    }
 
-	uncertainty()
-	{
-		main = 0;
-		uncert = 0;
-	}
+    uncertainty()
+    {
+    	main = 0;
+    	uncert = 0;
+    }
 
-	uncertainty operator +(const uncertainty &u)
-	{
-		return uncertainty(this->main + u.main,
-				sqrt(x2(this->uncert) + x2(u.uncert)));
-	}
-	uncertainty operator -(const uncertainty &u)
-	{
-		return uncertainty(this->main - u.main,
-				sqrt(x2(this->uncert) + x2(u.uncert)));
-	}
-	uncertainty operator *(const uncertainty &u)
-	{
-		TYPE _main = this->main * u.main;
-		TYPE _u = sqrt(x2(this->uncert / this->main) + x2(u.uncert / u.main));
-		return uncertainty(_main, _main * _u);
-	}
-	uncertainty operator /(const uncertainty &u)
-	{
-		TYPE _main = this->main / u.main;
-		TYPE _u = sqrt(x2(this->uncert / this->main) + x2(u.uncert / u.main));
-		return uncertainty(_main, _main * _u);
-	}
+    uncertainty operator +(const uncertainty &u)
+    {
+    	return uncertainty(this->main + u.main,
+    			sqrt(x2(this->uncert) + x2(u.uncert)));
+    }
+    uncertainty operator -(const uncertainty &u)
+    {
+    	return uncertainty(this->main - u.main,
+    			sqrt(x2(this->uncert) + x2(u.uncert)));
+    }
+    uncertainty operator *(const uncertainty &u)
+    {
+    	TYPE _main = this->main * u.main;
+    	TYPE _u = sqrt(x2(this->uncert / this->main) + x2(u.uncert / u.main));
+    	return uncertainty(_main, _main * _u);
+    }
+    uncertainty operator /(const uncertainty &u)
+    {
+    	TYPE _main = this->main / u.main;
+    	TYPE _u = sqrt(x2(this->uncert / this->main) + x2(u.uncert / u.main));
+    	return uncertainty(_main, _main * _u);
+    }
 
-	
-	uncertainty operator ^(const double n)
-	{
-		TYPE _main = pow(this->main, n);
-		TYPE _u = sqrt(x2(this->uncert / this->main * n));
-		return uncertainty(_main, _main * _u);
-	}
+    
+    uncertainty operator ^(const double n)
+    {
+    	TYPE _main = pow(this->main, n);
+    	TYPE _u = sqrt(x2(this->uncert / this->main * n));
+    	return uncertainty(_main, _main * _u);
+    }
 
-	uncertainty operator ^(const uncertainty n)
-	{
-		TYPE _main = pow(this->main, n.main);
-		TYPE _u = sqrt(x2(this->uncert / this->main * n.main));
-		return uncertainty(_main, _main * _u);
-	}
+    uncertainty operator ^(const uncertainty n)
+    {
+    	TYPE _main = pow(this->main, n.main);
+    	TYPE _u = sqrt(x2(this->uncert / this->main * n.main));
+    	return uncertainty(_main, _main * _u);
+    }
 
 };
 #endif
